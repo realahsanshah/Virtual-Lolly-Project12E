81870cf9ca32e268d6386da9bd51471c35b64ae8
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-16 23:49:03 +0500

    created basic lolly component

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import Lolly from '../components/Lolly'
+
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Virtual Lolly</title>
+      <h1>Hello World</h1>
+      <div>
+        <Lolly fillTop="#deaa43" fillBottom="#d52358" fillMiddle="#e95946"/>
+      </div>
+    </main>
+  )
+}
+
+export default IndexPage
