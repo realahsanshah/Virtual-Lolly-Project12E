740619d6b5f02816b291f9a42275b5e8657af5b1
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-17 00:07:50 +0500

    added add button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Header from "../components/Header"
 import Lolly from '../components/Lolly'
+import {navigate} from 'gatsby';
 
 
 const IndexPage = () => {
@@ -8,8 +9,25 @@ const IndexPage = () => {
     <div>
       <title>Virtual Lolly</title>
       <Header/>
-      <div>
-        <Lolly fillTop="#deaa43" fillBottom="#d52358" fillMiddle="#e95946"/>
+      <div className='lolly-list'>
+      <Lolly
+          fillTop="#6b6bde"
+          fillMiddle="#4ac383"
+          fillBottom="#d2ec27"
+        />
+        <Lolly
+          fillTop="#b71616"
+          fillMiddle="#bf10f1"
+          fillBottom="#10adf1"
+        />
+        <Lolly
+          fillTop="#ffc107"
+          fillMiddle="#00a97e"
+          fillBottom="#ec398f"
+        />
+      </div>
+      <div className='btn-wrapper'>
+        <button className='' onClick={() => navigate('/createNew')}>Send a Lolly to your friend</button>
       </div>
     </div>
   )
